e07685fe549a064f8c573372ac55d6e0d9d058b7
emran <imranhayredin89@gmail.com>
2024-05-20 10:24:15 +0300

    added home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import About from "./about/page";
 import Footer from "./components/footer";
+import HomePage from "./home/page";
 
 export default function Home() {
   return (
     <div>
+      <HomePage />
       <About />
-      <Footer />
+  
     </div>
    
   );
