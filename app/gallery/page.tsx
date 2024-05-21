46a0f3ee4d86635f933b8d4f7122dd37421c6df9
Alazar-da <alazar.damena01@gmail.com>
2024-05-21 15:59:55 +0300

    gallery page edited

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -3,11 +3,9 @@ const Gallery = ()=>{
 
     return(
         <div className="All">
-
-   
         <div className="accordian bg-white body">
-	<ul>
-		<li>
+	<ul className="flex lg:flex-row flex-col">
+		<li className="w-1/4">
 			<div className="image_title">
 				<a href="#">KungFu Panda</a>
 			</div>
@@ -15,7 +13,7 @@ const Gallery = ()=>{
 				<img src="http://thecodeplayer.com/uploads/media/3yiC6Yq.jpg"/>
 			</a>
 		</li>
-		<li>
+		<li className="w-1/4">
 			<div className="image_title">
 				<a href="#">Toy Story 2</a>
 			</div>
@@ -23,7 +21,7 @@ const Gallery = ()=>{
 				<img src="http://thecodeplayer.com/uploads/media/40Ly3VB.jpg"/>
 			</a>
 		</li>
-		<li>
+		<li className="w-1/4">
 			<div className="image_title">
 				<a href="#">Wall-E</a>
 			</div>
@@ -31,7 +29,7 @@ const Gallery = ()=>{
 				<img src="http://thecodeplayer.com/uploads/media/00kih8g.jpg"/>
 			</a>
 		</li>
-		<li>
+		<li className="w-1/4">
 			<div className="image_title">
 				<a href="#">Up</a>
 			</div>
@@ -39,7 +37,7 @@ const Gallery = ()=>{
 				<img src="http://thecodeplayer.com/uploads/media/2rT2vdx.jpg"/>
 			</a>
 		</li>
-		<li>
+		<li className="w-1/4">
 			<div className="image_title">
 				<a href="#">Cars 2</a>
 			</div>
